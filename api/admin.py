a7adb090ce114865cd587dd696011d6471311aef
pranvking <pranavkarmacharya@gmail.com>
2025-06-28 16:23:32 +0545

    last commit lhope

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from django.contrib import admin
 from .models import Account
+from .models import Product
 
 admin.site.register(Account)
-
+admin.site.register(Product)
--- api/models.py
+++ api/models.py
@@ -31,3 +31,13 @@ class Account(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='product_images/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
